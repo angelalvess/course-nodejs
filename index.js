9197a85ad650e611e37339d1528117bf30f62be6
angelalvess <angeltrashangel@gmail.com>
2024-03-18 12:32:25 -0300

    js added

---
+++ index.js
@@ -0,0 +1,4 @@
+const { Person } = require('./person.js');
+
+const person = new Person('John Doe');
+person.greet();
---
+++ person.js
@@ -0,0 +1,10 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  greet() {
+    console.log(`Hello, my name is ${this.name}`);
+  }
+}
+
+module.exports = { Person };
